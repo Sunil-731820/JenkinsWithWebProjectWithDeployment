7c5004f5874c10012a4c89e6eabb3a01c5201d93
sunilkumargupta <62325742+Sunil-731820@users.noreply.github.com>
2025-01-12 22:23:36 +0530

    Second jenkins project With Build And Deployment

--- src/main/java/com/java/mvn/ConnectionHelper.java
+++ src/main/java/com/java/mvn/ConnectionHelper.java
@@ -6,7 +6,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
 
-public class  {
+public class ConnectionHelper {
 	public static Connection getConnection() throws ClassNotFoundException, SQLException {
 		ResourceBundle rb = ResourceBundle.getBundle("db");
 		String driver =rb.getString("driver");
