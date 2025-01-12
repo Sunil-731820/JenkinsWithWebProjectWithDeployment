3eb12981dd500a7e1b542185113f9294b59b8d92
sunilkumargupta <62325742+Sunil-731820@users.noreply.github.com>
2025-01-12 21:21:14 +0530

    Second jenkins project With Build And Deployment

---
+++ src/main/java/com/java/mvn/AdminDAO.java
@@ -0,0 +1,46 @@
+package com.java.mvn;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class AdminDAO {
+	Connection connection ;
+	PreparedStatement pst;
+	
+	public String adminRegister(Admin admin) throws ClassNotFoundException, SQLException {
+		connection = ConnectionHelper.getConnection();
+		String cmd = "insert into Admin(username,password)"+
+				" values (?,?)";
+		pst = connection.prepareStatement(cmd);
+		pst.setString(1, admin.getUsername());
+		pst.setString(2, admin.getPassword());
+		pst.executeUpdate();
+		return "Admin Register Successfully";
+		
+	}
+	
+//	public String adminLoging(Admin admin) throws ClassNotFoundException, SQLException {
+//		connection = ConnectionHelper.getConnection();
+//		String cmd = "select *from adminreg";
+//		pst = connection.prepareStatement(cmd);
+//		
+//		
+//	}
+	public int adminLogIn(String username , String password) throws ClassNotFoundException, SQLException {
+		connection = ConnectionHelper.getConnection();
+		String cmd = "select count(*) cnt from Admin where username=? and password=?";
+		pst = connection.prepareStatement(cmd);
+		pst.setString(1, username);
+		pst.setString(2, password);
+		ResultSet res = pst.executeQuery();
+		res.next();
+		int count = res.getInt("cnt");
+		return count;
+	}
+	
+
+
+
+}
